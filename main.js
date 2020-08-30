21c8536c7e44f2b328b0511971433d230714eac7
Mohd. Intekhab <mohdintekhab96@gmail.com>
2020-08-30 22:11:03 +0530

    Adding Contact form

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var tabs = document.querySelectorAll(".tabs ul li");
-var tab_wraps = document.querySelectorAll(".tab_wrap");
+const tabs = document.querySelectorAll(".tabs ul li");
+const tab_wraps = document.querySelectorAll(".tab_wrap");
 
 tabs.forEach(function(tab, tab_index){
 	tab.addEventListener("click", function(){
@@ -9,7 +9,10 @@ tabs.forEach(function(tab, tab_index){
 		tab.classList.add("active");
 
 		tab_wraps.forEach(function(content, content_index){
-			if(content_index == tab_index){
+			if(tab_index == 4){
+				openModal();
+			}
+            else if(content_index == tab_index){
 				content.style.display = "block";
 			}
 			else{
@@ -19,3 +22,42 @@ tabs.forEach(function(tab, tab_index){
 
 	})
 })
+
+
+const inputs = document.querySelectorAll(".input");
+
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+
+const overlay = document.getElementById('overlay')
+const modal = document.getElementById("form");
+
+function openModal() {
+	if (modal == null) return;
+	modal.classList.add('active');
+	overlay.classList.add('active');
+	document.body.style.overflowY = "hidden";
+}
+
+function closeModal() {
+	if (modal == null) return
+	modal.classList.remove('active')
+	overlay.classList.remove('active');
+	document.body.style.overflowY = "auto";
+}
